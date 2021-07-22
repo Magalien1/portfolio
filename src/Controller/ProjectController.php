1eebb288bc409a40d84667549ebf9daedf504a5d
Magalien1 <emma.baldovino2@gmail.com>
2021-07-22 14:27:39 +0200

    show projects in modals done for work, i make design later

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ProjectController extends AbstractController
 {
@@ -13,8 +15,12 @@ class ProjectController extends AbstractController
      */
     public function index(): Response
     {
+        $projects = $this->getDoctrine()->getRepository(Project::class)->findAll();
+//        dd($projects);
         return $this->render('project/index.html.twig', [
             'controller_name' => 'ProjectController',
+            'projects' => $projects,
         ]);
     }
+
 }
