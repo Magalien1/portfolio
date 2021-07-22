05ad1c05d744ddad855249018d3884cbd1e00f08
Magalien1 <emma.baldovino2@gmail.com>
2021-07-22 15:32:59 +0200

    fixed some bug front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,10 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $projects = $this->getDoctrine()->getRepository(Project::class)->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'projects' => $projects,
         ]);
     }
 }
